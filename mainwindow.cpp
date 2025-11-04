d76523c7a29d4c8142eb2874ae9074f5e249358b
Dan <dbogardaustin@gmail.com>
2025-11-04 07:29:14 -0600

    Added horn output and started horn pushbuttion interrupt support.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -25,6 +25,7 @@ MainWindow::MainWindow(QWidget *parent)
     pinMode(BACK_RIGHT_INTERIOR_GPIO, OUTPUT);
     pinMode(BACK_LEFT_INTERIOR_GPIO, OUTPUT);
     pinMode(HEADLIGHTS_GPIO, OUTPUT);
+    pinMode(HORN_GPIO, OUTPUT);
 
     if (headlightsOn) {
         ui->headlights->setStyleSheet("background-color:rgb(255,255,192)");// on when true
@@ -79,12 +80,13 @@ void MainWindow::on_interiorLights_clicked()
 void MainWindow::on_Horn_pressed()
 {
     hornTimer->start();
+    ui->Horn->setStyleSheet("border-radius:80; border-color:rgb(0,0,0); border-style:solid; border-width:3; background-color:rgb(255,0,0)");
 //     softPwmCreate(HORN_GPIO, 0, 100);
 }
 
 void MainWindow::carHorn(){
     hornPWM = (hornPWM + 1) % 2;
-    qInfo() << "PWM SET TO: " << hornPWM << "\n";
+    qInfo() << "PWM SET TO: " << hornPWM;
     digitalWrite(HORN_GPIO, hornPWM);
 //    softPwmWrite(HORN_GPIO, hornPWM);
 //    if(hornPWM==0){
@@ -96,12 +98,33 @@ void MainWindow::carHorn(){
 }
 
 
-
-
 void MainWindow::on_Horn_released()
 {
     hornTimer->stop();
     hornPWM = 0;
     digitalWrite(HORN_GPIO, 0);
 //    softPwmWrite(HORN_GPIO, hornPWM);
+    ui->Horn->setStyleSheet("border-radius:80; border-color:rgb(0,0,0); border-style:solid; border-width:3; background-color: rgb(240, 240, 240)");
 }
+
+// Global pointer to Qt object (e.g. MainWindow)
+// This is necessary to emit a signal
+// static MyQtClass* s_myQtObject = nullptr;
+//
+//void myISRHandler(){
+//    if(s_mQtObject){
+//        //emit a Qt signal from the ISR
+//        // This must be done carefully to issues with threads and Qt's event loop.
+//        // Using a queued connection is often best.
+//        s_myObject->emitInterruptSignal();
+//    }
+//}
+//void setupWiringPiISR(int pin, MyQtClass* qtObject){
+//    wiringPiSetupGpio();
+//    pinMode(HORN_INT_GPIO, INPUT);
+//    pullUpDnControl(HORN_INT_GPIO, PUD_UP);
+//    s_myQtObject = qtObject; // Assign the Qt object pointer
+    // Register the ISR
+//    wiringPiISR(HORN_INT_GPIO, INT_EDGE_BOTH, &myISRHandler);
+//}
+
